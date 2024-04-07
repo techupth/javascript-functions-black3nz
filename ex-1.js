10cb30b6a1ac89e361c550b295c422f09e3a3f76
black3nz <pawich.p@hotmail.com>
2024-04-07 12:25:15 +0700

    Assignment submission

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,20 @@
-// Start coding here
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+console.log(add(3,5));
+console.log(subtract(3,5));
+console.log(multiply(3,5));
+console.log(divide(3,5));
--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,20 @@
-// Start coding here
+let add = function (a,b) {
+    return a + b;
+}
+
+let subtract = function (a,b) {
+    return a - b;
+}
+
+let multiply = function (a,b) {
+    return a * b;
+}
+
+let divide = function (a,b) {
+    return a / b;
+}
+
+console.log(add(3,5));
+console.log(subtract(3,5));
+console.log(multiply(3,5));
+console.log(divide(3,5));
